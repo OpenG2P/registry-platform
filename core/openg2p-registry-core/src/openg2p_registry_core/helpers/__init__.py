a2a9eced23a584ccb43ba87568aa1a961721f162
tahzeer <tahzeer.ash@gmail.com>
2026-07-01 10:00:22 +0530

    G2P-5245 feat(application-reference): add application reference field, its generation, usage and update api
    - implement application reference generator
    - update G2PIntakeForm to include application_reference
    - add new intake form record name generation abstract method
    - modify intake form services to handle application_reference
    - remove deprecated submission_reference_generator

--- core/openg2p-registry-core/src/openg2p_registry_core/helpers/__init__.py
+++ core/openg2p-registry-core/src/openg2p_registry_core/helpers/__init__.py
@@ -2,7 +2,7 @@ from .awe_config import get_awe_settings
 from .awe_helper import AWEClientError, AweHelper
 from .minio_client import MinioClient, MinioBucketEnum
 from .pattern_matcher import PatternMatcher
-from .submission_reference_generator import submission_reference_generator
+from .application_reference_generator import generate_application_reference
 from .template_helper import TemplateHelper
 from .websub_helper import WebsubHelper
 from .register_field_metadata import iter_register_orm_field_metadata
